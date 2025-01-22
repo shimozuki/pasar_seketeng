1aeb01f5e7301103c4cb5efef2fd2aed8678b30c
robbishimozuki <robbishimozuki@gmail.com>
2025-01-22 22:08:31 +0800

    l salah

--- app/Repositories/RetribusiRepository.php
+++ app/Repositories/RetribusiRepository.php
@@ -48,7 +48,7 @@ class RetribusiRepository extends Controller implements RetribusiInterface
         $retribusi = $retribusiQuery->get();
 
         $totalNunggak = Retribusi::where('status', 'nunggak')->sum('jumlah');
-        $totalLunas = Retribusi::where('status', 'Lunas')->sum('jumlah'); 
+        $totalLunas = Retribusi::where('status', 'lunas')->sum('jumlah'); 
 
         $namaPasar = Retribusi::select('pedagang.nama_pedagang')
         ->join('pedagang', 'retribusi.data_pasar_id', '=', 'pedagang.id')
